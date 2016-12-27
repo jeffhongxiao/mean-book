1ba79efc960a4b0353a4f9815593079e183f2e4d
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2016-12-27 10:02:54 -0400

    add angularjs application routes

--- public/application.js
+++ public/application.js
@@ -1,6 +1,13 @@
 var mainApplicationModuleName= 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+
+// config hashbang (#!), for better SEO
+mainApplicationModule.config(
+  ['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }]
+);
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainApplicationModuleName]);
