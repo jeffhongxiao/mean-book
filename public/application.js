05200d88c48ababb22020b4acab22dcef0b9a2b9
Jeff Hong Xiao <jeff.hong.xiao@gmail.com>
2016-12-31 21:58:54 -0400

    temp work to use html5Mode

--- public/application.js
+++ public/application.js
@@ -3,15 +3,32 @@ var mainApplicationModuleName= 'mean';
 var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngResource', 'ngRoute', 'users', 'example',
 'articles']);
 
-// config hashbang (#!), for better SEO
 mainApplicationModule.config(
   ['$locationProvider', function($locationProvider) {
-    $locationProvider.hashPrefix('!');
+    // XXX config hashbang (#!), for better SEO
+//    $locationProvider.hashPrefix('!');
+
+    // XXX use html5mode if possible
+    // ref: http://stackoverflow.com/a/38772825/3380951
+    if (window.history && window.history.pushState) {
+      console.log("locationProvider uses: html5Mode");
+      // $locationProvider.html5Mode({
+      //     enabled: true,
+      //     requireBase: true,
+      //     rewriteLinks: false
+      // });
+      $locationProvider.html5Mode(true);
+    }
+    else {
+      console.log("locationProvider uses: hashbang");
+      $locationProvider.html5Mode(false);
+    }
   }]
 );
 
-if (window.location.hash === '#_=_')
-  window.location.hash = '#!';
+// fix Facebook oauth bug
+//if (window.location.hash === '#_=_')
+//  window.location.hash = '#!';
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainApplicationModuleName]);
